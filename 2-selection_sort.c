1901bf350ea21c59151697d2d37c8d53426442c0
GracyDinma <okohagchidinma@gmail.com>
2024-05-04 11:16:57 -0700

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,23 +24,22 @@ void swap(int *c, int *d)
  */
 void selection_sort(int *array, size_t size)
 {
-	int min_idx;
-	size_t i, j;
-
-	if (array == NULL || size < 2)
+	if (array == NULL || size < 1)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (size_t i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
+		size_t min_idx = i;
+		for (size_t j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 			{
 				min_idx = j;
+			}
+
+			if (min_idx != i)
 			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
 			}
-		}
 	}
 }
